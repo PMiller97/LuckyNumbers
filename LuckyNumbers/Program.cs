e0b3c70c6208be46ab975598c8d0bb0ffa4afdf8
Paige Miller <paige_9753@hotmail.com>
2017-07-10 09:27:31 -0400

    finished project

---
+++ LuckyNumbers/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = 0;
+            //Console.WriteLine("Please enter a starting number");
+            //int numberStart = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter an ending number");
+            //int numberEnd = int.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine("JackPot amount is $795,000! Good Luck!");
+
+            //int[] userInput = new int[6];
+            //for (int i = 0; i < userInput.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter the 6 numbers you think will be the winners! ");
+            //    userInput[i] = int.Parse(Console.ReadLine());
+                
+
+            //    while (userInput[i] < numberStart || userInput[i] > numberEnd)
+            //    {
+            //        Console.WriteLine("Please enter a valid number!");
+            //        userInput[i] = int.Parse(Console.ReadLine());
+
+            //    }
+               
+            //}
+
+
+            
+
+
+            //Random luckyNumbers = new Random();
+            //int[] numbers = new int[6];// 6 numbers are being guessed
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = luckyNumbers.Next(numberStart, numberEnd);//min number and max number
+
+            //    Console.WriteLine("Lucky Number: {0}", numbers[i]);
+            //}
+
+            //for (int i = 0; i < numbers.Length; i ++) 
+            //{
+            //    for (int j = 0; j < userInput.Length; j++)
+            //    {
+            //        if(numbers[j] == userInput[i])
+            //        {
+            //            count++;//counting the number of matches
+            //        }
+            //    }
+            //}
+
+            
+
+            
+            //switch (count)
+            //{
+            //    case 0:
+            //        Console.WriteLine("You guessed 0 numbers correctly :(.");
+            //        Console.WriteLine("You did not win anything. Better luck next time!");
+            //        break;
+
+            //    case 1:
+            //        Console.WriteLine("You guessed 1 number correctly!");
+            //        Console.WriteLine("You won $30,000!");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("You guessed 2 numbers correctly!");
+            //        Console.WriteLine("You won $75,000!");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("You guessed 3 numbers correctly!");
+            //        Console.WriteLine("You won $125,000!");
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("You guessed 4 numbers correctly!");
+            //        Console.WriteLine("You won $175,000!");
+            //        break;
+
+            //    case 5:
+            //        Console.WriteLine("You guessed 5 numbers correctly!");
+            //        Console.WriteLine("You won $250,000!");
+            //        break;
+
+            //    case 6:
+            //        Console.WriteLine("You guessed all 6 numbers correctly!!");
+            //        Console.WriteLine("You won the jackpot! $795,000!!");
+            //        break;
+            //}
+
+
+            //Console.WriteLine("Do you want to play again? YES/NO");
+            //string playAgain = Console.ReadLine().ToUpper();// not case sensitive
+            //while (playAgain == "NO")
+            //{
+            //    Console.WriteLine("Thanks for playing!"); //thanks for playing runs a million times... why?
+            //}
+            //while (playAgain == "YES") ; //should restart program... doesn't
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
